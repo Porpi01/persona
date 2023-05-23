30bddcee7030cd2f0c6650f0a76b81c59457188a
macanyiz@gmail.com <macanyiz@gmail.com>
2023-05-23 20:06:19 +0200

    He añadido la función imprimirDatos

--- Persona.java
+++ Persona.java
@@ -11,3 +11,8 @@ return this.nombre;
 public int getEdad(){
 return this.edad;
 }
+
+public void imprimirDatos() {
+System.out.println("Nombre: " + nombre);
+System.out.println("Edad: " + nombre);
+}
